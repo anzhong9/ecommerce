2e106359c85da09c95f8a837b730ccf964e6d4d1
Devendra Neelam <110590941+anzhong9@users.noreply.github.com>
2023-12-19 14:05:14 +0530

    Footer position

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import Navbar from './pages/Navbar'
 import Home from './pages/Home'
+import Footer from './pages/Footer'
 function App() {
 
   return (
     <>
     <Navbar/>
     <Home/>
+    <Footer/>
     </>
   )
 }
